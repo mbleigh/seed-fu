6260b4a174ff69cc6fdf1818e1181dba03c4cab7
Jacques Crocker <jcnetdev@gmail.com>
2008-07-04 12:01:42 -0700

    Fixing ActiveRecord override

--- lib/seed-fu.rb
+++ lib/seed-fu.rb
@@ -62,3 +62,15 @@ module SeedFu
     end
   end
 end
+
+
+class ActiveRecord::Base
+  # Creates a single record of seed data for use
+  # with the db:seed rake task. 
+  # 
+  # === Parameters
+  # constraints :: Immutable reference attributes. Defaults to :id
+  def self.seed(*constraints, &block)
+    SeedFu::Seeder.plant(self, *constraints, &block)
+  end
+end
