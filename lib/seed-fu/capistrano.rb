e46d1981e016898d735afd1715f5f1e44b6273fd
Jun Lin <linjunpop@gmail.com>
2013-11-14 09:39:32 +0800

    Run :seed_fu only on primary db server.

--- lib/seed-fu/capistrano.rb
+++ lib/seed-fu/capistrano.rb
@@ -1,7 +1,7 @@
 Capistrano::Configuration.instance.load do
   namespace :db do
     desc "Load seed data into database"
-    task :seed_fu do
+    task :seed_fu, :roles => :db, :only => { :primary => true } do
       run "cd #{release_path} && bundle exec rake RAILS_ENV=#{rails_env} db:seed_fu"
     end
   end
